65478a9ea9387b314238fbbb958dfb49b4475546
Mustafa Kurukamci <isbuan51@gmail.com>
2020-09-12 07:38:27 +0200

    Updates

---
+++ precache-manifest.fe936d04a075f9106888fca4899c20fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5beb9cf6276e9d694bfcd000da0aac8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aeefbdb4997924e55cf",
+    "url": "/static/css/2.209c414f.chunk.css"
+  },
+  {
+    "revision": "be7cfd8f7b28f5967053",
+    "url": "/static/css/main.340fc15a.chunk.css"
+  },
+  {
+    "revision": "5aeefbdb4997924e55cf",
+    "url": "/static/js/2.a42a8e5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a42a8e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7cfd8f7b28f5967053",
+    "url": "/static/js/main.d359972d.chunk.js"
+  },
+  {
+    "revision": "b9fd4786766e8e8819e3",
+    "url": "/static/js/runtime-main.9a0e6d6b.js"
+  }
+]);
